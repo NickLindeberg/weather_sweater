1faca252e9fcbbb2c71b3f4fb13a42dd3268960b
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-26 16:03:55 -0700

    adds cache to controller

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundController < ApplicationController
 
   def show
+    expires_in 60.minutes, :public => true
     render json: BackgroundSerializer.new(BackgroundBuilder.new(params[:location]))
   end
 
--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
 
   def show
+    expires_in 60.minutes, :public => true
     render json: ForecastSerializer.new(ForecastBuilder.new(params[:location]))
   end
 
