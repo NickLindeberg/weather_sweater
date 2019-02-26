b3d6842a044d208778c5935f13fee02ef0639382
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-26 15:35:36 -0700

    fixed botched attempt at caching

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -8,10 +8,7 @@ class DarkskyService
 
   def connect
     Faraday.new(url:"https://api.darksky.net/") do |faraday|
-      faraday.use :http_cache, store: Rails.cache, logger: ActiveSupport::Logger.new(STDOUT)
       faraday.adapter Faraday.default_adapter
-      faraday.response :json, :content_type => /\bjson$/
-      faraday.use Faraday::OverrideCacheControl, cache_control: ‘public, max-age=3600’
     end
   end
 
